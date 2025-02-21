94cadf9f74d4addf858b5f4a923aad2d221537d4
abateagegnehu <abateagegnehu574@gmail.com>
2025-02-21 11:40:33 +0300

    update

--- server.js
+++ server.js
@@ -2,16 +2,14 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
-const authRoutes = require("./routes/authRoutes");
-const taskRoutes = require("./routes/taskRoutes");
+const productRoutes = require("./routes/product.route");
 
 const app = express();
 connectDB();
 app.use(express.json());
 app.use(cors());
 
-app.use("/api/auth", authRoutes);
-app.use("/api/tasks", taskRoutes);
+app.use("/api/products", productRoutes);
 
 if (!process.env.MONGO_URI || !process.env.JWT_SECRET || !process.env.PORT) {
   console.error("Missing required environment variables.");
