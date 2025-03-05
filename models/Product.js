4e958c3f45eb7e32c4b165954637d1d7a502f9fb
Abate <147659641+abate-Agegnehu@users.noreply.github.com>
2025-03-05 09:45:47 +0300

    Update Product.js

--- models/Product.js
+++ models/Product.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 
 const ProductSchema = new mongoose.Schema({
   type: { type: String, required: true },
-  price: { type: Number, required: true },
   description: { type: String, required: true },
   imageUrl: { type: String, required: true },
   cloudinaryId: { type: String, required: true },
